69b00572244f97bb78ecb3f4d83be64944e47527
Starrier <1342878298@qq.com>
2018-06-05 01:04:08 +0800

    Swagger APIs Docs

---
+++ src/main/java/org/starrier/dreamwar/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package org.starrier.dreamwar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author Starrier
+ * @Time 2018/6/5.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket buildDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xingguo.springboot.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInf(){
+        return new ApiInfoBuilder()
+                .title("DreamWar-Backend")
+                .description("DreamWar RESTful APIs Docs")
+                .termsOfServiceUrl("http://dream.starriers.com")
+                .contact(new Contact("Starrier", "https://github.com/Starrier", "starrierc@gmail.com"))
+                .build();
+
+    }
+}
