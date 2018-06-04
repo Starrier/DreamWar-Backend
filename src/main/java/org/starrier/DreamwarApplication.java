45b0c266c50d2eac29c8a19e5faba41fef445220
Starrier <1342878298@qq.com>
2018-06-05 00:53:37 +0800

    add actuator

--- src/main/java/org/starrier/DreamwarApplication.java
+++ src/main/java/org/starrier/DreamwarApplication.java
@@ -1,12 +1,22 @@
 package org.starrier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DreamwarApplication {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(DreamwarApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreamwarApplication.class, args);
+
+		LOGGER.info("------------ DreamWar-Backend -------- Have Started ! -----");
 	}
+
+
 }
