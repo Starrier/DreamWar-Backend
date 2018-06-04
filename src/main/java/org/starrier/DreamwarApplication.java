936b1686ff8f9259b1e940b79bed3d0306303b67
Starrier <1342878298@qq.com>
2018-06-05 01:44:19 +0800

    Druid

--- src/main/java/org/starrier/DreamwarApplication.java
+++ src/main/java/org/starrier/DreamwarApplication.java
@@ -4,10 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan
+@ImportResource(locations = { "classpath:druid/druid-bean.xml" })
 public class DreamwarApplication {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(DreamwarApplication.class);
